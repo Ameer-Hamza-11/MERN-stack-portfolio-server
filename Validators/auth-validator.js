400c4228910ee1c30cc212e0540628f1cb4dc9a9
Ameer-Hamza-10 <ameerhamza.reactjs@gmail.com>
2025-11-15 12:09:24 +0500

    minor issues solved

--- Validators/auth-validator.js
+++ Validators/auth-validator.js
@@ -1,52 +1,44 @@
 const { z } = require('zod')
 
-
 const signupValidation = z.object({
-    username: z
-        .string({ required_error: 'Username is required' })
+    username: z.string({ required_error: 'Username is required' })
         .trim()
-        .min(3, { message: 'Username must be at least 3 characters long' })
-        .max(20, { message: 'Username must be at most 20 characters long' }),
-    email: z
-        .string({ required_error: 'Email is required' })
+        .min(3, 'Username must be at least 3 characters long')
+        .max(20, 'Username must be at most 20 characters long'),
+
+    email: z.string({ required_error: 'Email is required' })
         .trim()
-        .email({ message: 'Invalid email format' })
-        .min(3, { message: 'Email must be at least 3 characters long' })
-        .max(25, { message: 'Email must be at most 25 characters long' })
-        .refine(val => val.endsWith('.com'), {
+        .email('Invalid email format')
+        .min(3, 'Email must be at least 3 characters long')
+        .max(30, 'Email must be at most 30 characters long')
+        .refine((val) => val.endsWith('.com'), {
             message: 'Email must end with .com'
         }),
-    phone: z
-        .string({ required_error: 'phone Number is required' })
-        .trim()
-        .min(11, { message: 'phone Number must be at least 11 characters long' })
-        .max(20, { message: 'phone Number must be at most 20 characters long' })
-        .regex(/^[0-9]{11}$/, {
-            message: 'Phone number must be exactly 11 digits'
-        }),
-    password: z
-        .string({ required_error: 'password is required' })
-        .trim()
-        .min(6, { message: 'password must be at least 6 characters long' })
-        .max(1024, { message: 'password must be at most 1024 characters long' }),
 
+    phone: z.string({ required_error: 'Phone number is required' })
+        .trim()
+        .regex(/^[0-9]{11}$/, 'Phone number must be exactly 11 digits'),
+
+    password: z.string({ required_error: 'Password is required' })
+        .trim()
+        .min(6, 'Password must be at least 6 characters long')
+        .max(1024, 'Password must be at most 1024 characters long'),
 })
+
 const signinValidation = z.object({
-    email: z
-        .string({ required_error: 'Email is required' })
+    email: z.string({ required_error: 'Email is required' })
         .trim()
-        .email({ message: 'Invalid email format' })
-        .min(3, { message: 'Email must be at least 3 characters long' })
-        .max(25, { message: 'Email must be at most 25 characters long' })
+        .email('Invalid email format')
+        .min(3, 'Email must be at least 3 characters long')
+        .max(30, 'Email must be at most 30 characters long')
         .refine(val => val.endsWith('.com'), {
             message: 'Email must end with .com'
         }),
-    password: z
-        .string({ required_error: 'password is required' })
-        .trim()
-        .min(6, { message: 'password must be at least 6 characters long' })
-        .max(1024, { message: 'password must be at most 1024 characters long' }),
-})
 
+    password: z.string({ required_error: 'Password is required' })
+        .trim()
+        .min(6, 'Password must be at least 6 characters long')
+        .max(1024, 'Password must be at most 1024 characters long'),
+})
 
 module.exports = { signupValidation, signinValidation }
